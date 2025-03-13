f08732a6ed7b15ef899334ec1b1cda820292ff41
ChuckNoelke <marnoelke@googlemail.com>
2025-03-13 07:46:35 +0100

    server actions

---
+++ road-to-next/src/features/ticket/actions/delete-ticket.ts
@@ -0,0 +1,13 @@
+'use server';
+import {prisma} from "@/lib/prisma";
+
+
+const deleteTicket= async (id: string)=>{
+
+    await prisma.ticket.delete({
+        where: {id,}
+    });
+};
+
+
+export {deleteTicket};
--- road-to-next/src/features/ticket/components/ticket-item.tsx
+++ road-to-next/src/features/ticket/components/ticket-item.tsx
@@ -1,58 +1,65 @@
+import { Ticket } from "@prisma/client";
 import clsx from "clsx";
-import {LucideSquareArrowOutUpRight} from "lucide-react"
+import { LucideArrowUpRightFromSquare, LucideTrash } from "lucide-react";
 import Link from "next/link";
-import {Button} from "@/components/ui/button";
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {TICKET_ICONS} from "@/features/ticket/constants";
-import {Ticket} from "@/features/ticket/types";
-import {ticketPath} from "@/paths";
-
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
+import { TICKET_ICONS } from "../constants";
 
 type TicketItemProps = {
-    ticket: Ticket,
-    isDetail?: boolean
+    ticket: Ticket;
+    isDetail?: boolean;
+};
 
-}
-
-const TicketItem = ({ticket, isDetail }: TicketItemProps) => {
-
-    console.log("Where am I displayed? (TicketItem)");
+const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
     const detailButton = (
-    <Button variant="outline" size="icon" asChild>
-        <Link href={ticketPath(ticket.id)} >
-            <LucideSquareArrowOutUpRight className="h-4 w-4"/>
-        </Link>
-    </Button>
-    )
+        <Button variant="outline" size="icon" asChild>
+            <Link href={ticketPath(ticket.id)}>
+                <LucideArrowUpRightFromSquare className="h-4 w-4" />
+            </Link>
+        </Button>
+    );
 
-    return(
-        <div className={clsx("w-full max-w-[420px] flex gap-x-1",
-            {
-                "[580px]": isDetail,
-                "[420px]": !isDetail
-            }
-            )}>
-            <Card key={ticket.id} className="w-full max-w">
+    const deleteButton = (
+        <form action={deleteTicket.bind(null, ticket.id)}>
+            <Button variant="outline" size="icon">
+                <LucideTrash className="h-4 w-4" />
+            </Button>
+        </form>
+    );
+
+    return (
+        <div
+            className={clsx("w-full flex gap-x-1", {
+                "max-w-[580px]": isDetail,
+                "max-w-[420px]": !isDetail,
+            })}
+        >
+            <Card className="w-full">
                 <CardHeader>
-                    <CardTitle className="flex gap-x-2 text-lg font-semibold">
+                    <CardTitle className="flex gap-x-2">
                         <span>{TICKET_ICONS[ticket.status]}</span>
                         <span className="truncate">{ticket.title}</span>
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
-                  <span className={clsx("whitespace-break-spaces",{
-                      "line-clamp-3": !isDetail
-                  })}>
-                    {ticket.content}
-                  </span>
+          <span
+              className={clsx("whitespace-break-spaces", {
+                  "line-clamp-3": !isDetail,
+              })}
+          >
+            {ticket.content}
+          </span>
                 </CardContent>
-
             </Card>
-            {isDetail ? null : (<div className="flex flex-col gap-y-1">
-                    {detailButton}
-                </div>)}
-        </div>
-)
-}
 
-export {TicketItem};
+            <div className="flex flex-col gap-y-1">
+                {isDetail ? deleteButton : detailButton}
+            </div>
+        </div>
+    );
+};
+
+export { TicketItem };
